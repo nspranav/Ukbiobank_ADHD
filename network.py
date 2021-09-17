c8a1e59d5c38656fb505ba9fda25d95d42567d47
pnadigapusuresh1 <pnadigapusuresh1@trendsgndev101.rs.gsu.edu>
2021-09-16 23:40:45 -0400

    Job 3891274 -Network with 3 CNN -Stride of 2

--- network.py
+++ network.py
@@ -6,17 +6,17 @@ class Network(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.cv1 = nn.Conv3d(1, 16, 3, stride=(1,1,1), padding=0) #try increasing filter size
+        self.cv1 = nn.Conv3d(1, 16, 3, stride=2, padding=0) #try increasing filter size
         self.bn1 = nn.BatchNorm3d(16)
-        self.cv2 = nn.Conv3d(16, 32, 3, stride=(1,1,1), padding=0) #try increase filter size
+        self.cv2 = nn.Conv3d(16, 32, 3, stride=2, padding=0) #try increase filter size
         self.bn2 = nn.BatchNorm3d(32)
-        self.cv3 = nn.Conv3d(32, 64, 3,stride=(1,1,1), padding=0)
+        self.cv3 = nn.Conv3d(32, 64, 3,stride=2, padding=0)
         self.bn3 = nn.BatchNorm3d(64)
-        self.cv4 = nn.Conv3d(64, 128, 3,stride=(1,1,1), padding=0)
+        self.cv4 = nn.Conv3d(64, 128, 3,stride=2, padding=0)
         self.bn4 = nn.BatchNorm3d(128)
-        self.cv5 = nn.Conv3d(128, 128, 3,stride=(1,1,1), padding=0)
+        self.cv5 = nn.Conv3d(128, 128, 3,stride=2, padding=0)
         self.bn5 = nn.BatchNorm3d(128)
-        self.cv6 = nn.Conv3d(128, 64, 1,stride=(1,1,1), padding=0)
+        self.cv6 = nn.Conv3d(128, 64, 1,stride=2, padding=0)
         self.bn6 = nn.BatchNorm3d(64)
         self.cv7 = nn.Conv3d(64,64,1) 
         self.pool = nn.MaxPool3d(2)
@@ -32,14 +32,14 @@ class Network(nn.Module):
         img = self.pool(F.relu(self.bn1(self.cv1(img))))
         img = self.pool(F.relu(self.bn2(self.cv2(img))))
         img = self.pool(F.relu(self.bn3(self.cv3(img))))
-        img = self.pool(F.relu(self.bn4(self.cv4(img))))
-        img = self.pool(F.relu(self.bn5(self.cv5(img))))
+        #img = self.pool(F.relu(self.bn4(self.cv4(img))))
+        #img = self.pool(F.relu(self.bn5(self.cv5(img))))
         #img = self.pool(F.relu(self.bn6(self.cv6(img))))
         #img = self.cv7(self.dropout(self.avgpool(img)))
 
         img = img.view(img.shape[0], -1)
-        img = self.dropout(F.relu(self.fc1(img)))
-        img = self.dropout(F.relu(self.fc2(img)))
+        # img = self.dropout(F.relu(self.fc1(img)))
+        # img = self.dropout(F.relu(self.fc2(img)))
         img = self.dropout(F.relu(self.fc3(img)))
         img = self.dropout(F.relu(self.fc4(img)))
 
